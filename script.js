77ccd1ad99c1a6c56a876e0b6fb000e01bd0f131
kgourav1 <kgourav1998@gmail.com>
2026-04-29 22:21:37 +0530

    timer reverted

--- script.js
+++ script.js
@@ -55,9 +55,8 @@ timeBtns.forEach((btn) => {
 
 function updateClock() {
   const now = new Date();
-  // const hours = String(now.getHours()).padStart(2, "0");
-  const hours = "02";
-  const minutes = "00"; //String(now.getMinutes()).padStart(2, "0");
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
   const seconds = String(now.getSeconds()).padStart(2, "0");
   timeDisplay.textContent = `${hours}:${minutes}:${seconds} AM`;
 }
